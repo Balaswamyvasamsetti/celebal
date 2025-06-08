55d8b35c24192b512b790dc6b05fd321f83872c8
balaswamy <balaswamyvasamsetti@gmail.com>
2025-06-08 10:23:52 +0530

    first commit

---
+++ helloworld/app.js
@@ -0,0 +1,12 @@
+/* 
+firstly make the directory mkdir helloworld
+then create the file  app.js
+then write the code to print the helloworld 
+then run the code node app.js
+then check the output in the terminal  hello world
+*/
+
+console.log("Hello World");
+
+
+
